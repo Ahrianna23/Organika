7f0ccc1c2cc1ae1e8b9d997dba1225620273823d
Ahrianna23 <34588621+Ahrianna23@users.noreply.github.com>
2020-09-17 16:22:21 -0400

    Add files via upload

---
+++ Organika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Organika
+//
+//  Created by Ahrianna Daniels on 9/17/20.
+//  Copyright © 2020 Ahrianna Daniels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
