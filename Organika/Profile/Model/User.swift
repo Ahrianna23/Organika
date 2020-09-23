2964437e7ceae3d63261b1c6694319572556a275
ArtemVaniukov <65284766+ArtemVaniukov@users.noreply.github.com>
2020-09-23 16:50:06 +0800

    Initialized User and Store Models

---
+++ Organika/Profile/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Organika
+//
+//  Created by Artem Vaniukov on 23.09.2020.
+//  Copyright © 2020 Ahrianna Daniels. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    /// What else do we need here?
+    var firstName: String?
+    var secondName: String?
+    var phoneNumber: String?
+    
+    var store: Store?
+    
+}
---
+++ Organika/Store/Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  Organika
+//
+//  Created by Artem Vaniukov on 23.09.2020.
+//  Copyright © 2020 Ahrianna Daniels. All rights reserved.
+//
+
+import UIKit
+
+struct Item {
+    
+    /// What else do we need here?
+    var name: String
+    var description: String
+    var price: Float
+    var image: UIImage
+    
+}
